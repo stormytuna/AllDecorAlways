b46f6400a67d52eb1726b9f3f0851cd71936edad
stormytuna <stormytuna@outlook.com>
2023-12-10 15:35:45 +0000

    Add project files.

---
+++ AllDecorAlways/Patches/TerminalPatch.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+
+namespace AllDecorAlways.Patches
+{
+    [HarmonyPatch(typeof(Terminal))]
+    public class TerminalPatch
+    {
+        [HarmonyPrefix, HarmonyPatch("RotateShipDecorSelection")]
+        public static bool AddAllShipDecor(Terminal __instance) {
+            IEnumerable<TerminalNode> availableDecor = StartOfRound.Instance.unlockablesList.unlockables
+                .Where(u => u.shopSelectionNode != null && !u.alwaysInStock && !u.hasBeenUnlockedByPlayer && !u.alreadyUnlocked)
+                .Select(u => u.shopSelectionNode);
+
+            foreach (var unlockable in availableDecor) {
+                AllDecorAlwaysBase.Log.LogInfo($"Thing is {unlockable.name}");
+            }
+
+            __instance.ShipDecorSelection = availableDecor.ToList();
+
+            AllDecorAlwaysBase.Log.LogInfo("Decor for everyone!");
+
+            return false; // No point running vanilla logic as we want to completely replace it
+        }
+
+        [HarmonyPostfix, HarmonyPatch("Awake")]
+        public static void ChangeDecorHeading(Terminal __instance) {
+            var storeNode = __instance.terminalNodes.allKeywords.First(kw => kw.name == "Store").specialKeywordResult;
+            var displayTextLines = storeNode.displayText.Split('\n').ToList();
+            var decorStartIndex = displayTextLines.IndexOf("[unlockablesSelectionList]");
+
+            if (decorStartIndex < 0) {
+                AllDecorAlwaysBase.Log.LogError("Failed to find decor start index in store node display text!");
+                return;
+            }
+
+            displayTextLines[decorStartIndex - 2] = "The Company appreciates your hard work, all ship decor is available:";
+            storeNode.displayText = string.Join("\n", displayTextLines);
+        }
+    }
+}
---
+++ AllDecorAlways/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace AllDecorAlways
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class AllDecorAlwaysBase : BaseUnityPlugin
+    {
+        public const string ModGUID = "stormytuna.AllDecorAlways";
+        public const string ModName = "AllDecorAlways";
+        public const string ModVersion = "1.0.0";
+
+        public static ManualLogSource Log = BepInEx.Logging.Logger.CreateLogSource(ModGUID);
+        public static AllDecorAlwaysBase Instance;
+
+        private readonly Harmony harmony = new Harmony(ModGUID);
+
+        private void Awake() {
+            if (Instance is null) {
+                Instance = this;
+            }
+
+            Log.LogInfo("All Decor Always has awoken!");
+
+            harmony.PatchAll();
+        }
+    }
+}
